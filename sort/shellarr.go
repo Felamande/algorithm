39094f7420cacb60ac324094cce8f1f6aef1bd71
Felamande <tangzhuohao@hotmail.com>
2018-10-18 19:41:38 +0800

    func genSeq

--- sort/shellarr.go
+++ sort/shellarr.go
@@ -6,21 +6,35 @@ import (
 )
 
 func main() {
-	h := 0
-	var a []int
-	for i := 0; h < math.MaxInt32; i++ {
-		h = int(math.Ceil(math.Pow(2.48, float64(i))))
-		a = append(a, h)
-	}
-	invert(a)
-	for _, v := range a {
-		print(v, ",")
-	}
+	a := genSeq(seqt(3))
+	printA(a)
 }
 
 func invert(a []int) {
 	l := len(a)
 	for i := 0; i <= l/2; i++ {
 		a[i], a[l-i-1] = a[l-i-1], a[i]
+	}
+}
+
+func printA(a []int) {
+	for _, v := range a {
+		print(v, ",")
+	}
+}
+
+func genSeq(f func(k int) int) (a []int) {
+	h := 0
+	for k := 0; h <= math.MaxInt32; k++ {
+		h = f(k)
+		a = append(a, h)
+	}
+	invert(a)
+	return a
+}
+
+func seqt(t float64) func(k int) int {
+	return func(k int) int {
+		return int(math.Floor(math.Pow(t, float64(k))))
 	}
 }
